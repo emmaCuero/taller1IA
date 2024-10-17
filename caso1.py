00011633f053bedee8799443e72a8235d2e92250
Emmanuel Cuero <manuelfc1990@gmail.com>
2024-10-17 18:36:53 -0500

    nuevo

--- caso1.py
+++ caso1.py
@@ -1,4 +1,5 @@
-""" def impares_inverso(inicio, fin):
+#caso 1
+def impares_inverso(inicio, fin):
     # Generamos los números impares dentro del rango, recorriendo el rango inversamente
     for numero in range(fin, inicio - 1, -1):
         if numero % 2 != 0:  # Condición para verificar si es impar
@@ -6,8 +7,8 @@
 
 # Ejemplo: imprimir los números impares entre 1 y 20 de manera inversa
 impares_inverso(1, 20)
- """
 
+#caso 2
 
 def impares_inverso(inicio, fin):
     impares = [str(numero) for numero in range(fin, inicio - 1, -1) if numero % 2 != 0]
